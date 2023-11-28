75265151437e62f8d096ef9d437e294821d1db8a
SAI CHETHAN <chethanaddetla@gmail.com>
2023-11-28 14:11:09 +0530

    Updates

---
+++ precache-manifest.a6179c5c8ff114ae101be3b32a3efc4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8ffca08bc7c466a27982234f333e80",
+    "url": "/Text-Work/index.html"
+  },
+  {
+    "revision": "cb6e9565261253660690",
+    "url": "/Text-Work/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/Text-Work/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Text-Work/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "cb6e9565261253660690",
+    "url": "/Text-Work/static/js/main.4c64f201.chunk.js"
+  },
+  {
+    "revision": "f3ecd25ddb88cbf663a9",
+    "url": "/Text-Work/static/js/runtime~main.ec1ccef5.js"
+  }
+]);
